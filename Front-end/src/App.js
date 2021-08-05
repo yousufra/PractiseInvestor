86b2d0c04f294aad2012ca86556afbbc31bc0f60
Joseph Kranak <jakranak@gmail.com>
2021-08-05 12:43:49 -0500

    fix: refactored Homepage and Navbar to Typescript

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -3,7 +3,7 @@
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Box } from '@material-ui/core';
-import Homepage from './components/Homepage/homepage';
+import Homepage from './components/Homepage/Homepage';
 import Authenticate from './components/Authenticate/authenticate';
 import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
