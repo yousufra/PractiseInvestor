2d97cda06c6bab8da45ad5469241033fa0604d8a
Joseph Kranak <jakranak@gmail.com>
2021-08-05 18:36:21 -0500

    files: utils/TablePagination

--- Front-end/src/components/PastActivities/PastActivities.tsx
+++ Front-end/src/components/PastActivities/PastActivities.tsx
@@ -1,88 +1,11 @@
 /* eslint-disable */
-import { useEffect, MouseEvent, useState } from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import {Table, TableBody, TableCell, TableContainer, TableFooter, TablePagination, TableHead, TableRow, Paper, IconButton, Box} from '@material-ui/core';
-import { KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import LastPageIcon from '@material-ui/icons/LastPage';
+import { MouseEvent, useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Table, TableBody, TableCell, TableContainer, TableFooter, TablePagination, TableHead, TableRow, Paper, Box} from '@material-ui/core';
 import { useSelector, useDispatch } from 'react-redux';
 import { PastActivity } from './PastActivity/PastActivity';
 import { getAllActivities } from '../../actions/activity';
-
-const useStyles1 = makeStyles((theme) => ({
-  root: {
-    flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
-  },
-}));
-
-interface Props {
-  count: number;
-  page: number;
-  rowsPerPage: number;
-  onPageChange: (event: MouseEvent<HTMLButtonElement>, newPage: number) => void;
-}
-
-function TablePaginationActions({ count, page, rowsPerPage, onPageChange }: Props) {
-  // determine types after done with tsx
-  console.log(count, page, rowsPerPage, onPageChange);
-
-  const classes = useStyles1();
-  const theme = useTheme();
-
-  const handleFirstPageButtonClick = (event: MouseEvent<HTMLElement>) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event: MouseEvent<HTMLElement>) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event: MouseEvent<HTMLElement>) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event: MouseEvent<HTMLElement>) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <div className={classes.root}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
-        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </div>
-  );
-}
-
-TablePaginationActions.propTypes = {
-  count: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
+import TablePaginationActions from '../../utils/TablePaginationActions';
 
 const useStyles2 = makeStyles({
   table: {
@@ -105,11 +28,11 @@ export default function CustomPaginationActionsTable() {
 
   const emptyRows = rowsPerPage - Math.min(rowsPerPage, activities.length - page * rowsPerPage);
 
-  const handleChangePage = (event, newPage) => {
+  const handleChangePage = (_: any, newPage: number) => {
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event) => {
+  const handleChangeRowsPerPage = (event: MouseEvent<HTMLDivElement>) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
