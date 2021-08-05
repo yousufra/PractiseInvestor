8a4fed86a31dc845f2ee444b9ea424a6fa4811be
biancahpp <bianca_procopio@hotmail.com>
2021-08-05 13:55:14 -0600

    fix: pastactivity type fixed

--- Front-end/src/components/PastActivities/PastActivity/PastActivity.tsx
+++ Front-end/src/components/PastActivities/PastActivity/PastActivity.tsx
@@ -1,24 +1,26 @@
 /* eslint-disable */
 import { TableRow, TableCell } from '@material-ui/core'
+import { StockI } from '../../../interfaces/Stock';
 
 interface Props {
-  activity: any
+  activity: StockI;
 }
-// fix any type
 
-const PastActivity = ({ activity }: Props) => (
-  <TableRow>
-    <TableCell component="th" scope="activity">
-      {activity.date}
-    </TableCell>
-    <TableCell align="right">{activity.company}</TableCell>
-    <TableCell align="right">{activity.ticker}</TableCell>
-    <TableCell align="right">{activity.action}</TableCell>
-    <TableCell align="right">{activity.quantity}</TableCell>
-    <TableCell align="right">${activity.price}</TableCell>
-    <TableCell align="right">${activity.netAmount}</TableCell>
-  </TableRow>
-);
+const PastActivity = ({ activity }: Props) => {
+  return (
+    <TableRow>
+      <TableCell component="th" scope="activity">
+        {activity.date}
+      </TableCell>
+      <TableCell align="right">{activity.company}</TableCell>
+      <TableCell align="right">{activity.ticker}</TableCell>
+      <TableCell align="right">{activity.action}</TableCell>
+      <TableCell align="right">{activity.quantity}</TableCell>
+      <TableCell align="right">${activity.price}</TableCell>
+      <TableCell align="right">${activity.netAmount}</TableCell>
+    </TableRow>
+
+  )
+};
 
 export default PastActivity;
-
