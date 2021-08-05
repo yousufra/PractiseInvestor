1a9181d5433584ee4778c52e9b3415b45d30c84b
Joseph Kranak <jakranak@gmail.com>
2021-08-05 09:44:35 -0500

    fix: PrivateRoute filename

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 import Homepage from './components/Homepage/homepage';
 import Authenticate from './components/Authenticate/authenticate';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import PrivateRoute from './components/PrivateRoute/privateRoute';
 
 function App() {
   return (
