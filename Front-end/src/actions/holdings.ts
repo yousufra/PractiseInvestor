3b95e18fb1ddc13082a009315fa7dda850087bc1
Joseph Kranak <jakranak@gmail.com>
2021-08-05 19:39:35 -0500

    fix: refactored actions folder

--- Front-end/src/actions/holdings.ts
+++ Front-end/src/actions/holdings.ts
@@ -1,9 +1,11 @@
 import { FETCH_HOLDINGS, UPDATE_HOLDING } from '../constants/actionTypes';
 import { getUser, putHoldings } from '../api/backendApi';// import all exports from api folder (EX, api.(function) to use a function )
+import { HoldingI } from '../interfaces/Holding';
+import { StockI } from '../interfaces/Stock'
 
 // Action Creators - functions that return an action
 
-export const getAllHoldings = () => async (dispatch) => {
+export const getAllHoldings = () => async (dispatch: (arg: { type: string; payload: { holdings: HoldingI[]; cash: number; }; }) => void) => {
   try {
     const { data } = await getUser(); // decontruct response to grab data from response object
 
@@ -19,7 +21,7 @@ export const getAllHoldings = () => async (dispatch) => {
   }
 };
 
-export const updateHoldings = (order) => async (dispatch) => {
+export const updateHoldings = (order: StockI) => async (dispatch: (arg: { type: string; payload: { holdings: HoldingI; cash: number; }; }) => void) => {
   try {
     const { data } = await putHoldings(order);
     const { holdings, cash } = data;
