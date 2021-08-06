5c50e208d575712f586eacd9c32b98f1f6c8c140
Sarah Shaffer <sarahsavell2@gmail.com>
2021-08-06 10:11:53 -0500

    just merge

--- Front-end/src/reducers/activitiesReducer.ts
+++ Front-end/src/reducers/activitiesReducer.ts
@@ -1,33 +1,6 @@
 import { IReduxBaseAction, EReduxActionTypes } from '../constants/actionTypes';
 
-<<<<<<< HEAD
 const activitiesReducer = (activities: any[], action:IReduxBaseAction) => { // need to inialize state(holdings) , holdings is gunna be an array of objects, reducer returns the new state
-=======
-// eslint-disable-next-line max-len
-/*
- export interface IReduxGetMoviesAction extends IReduxBaseAction {
-  type: EReduxActionTypes.GET_MOVIES;
-  data: IMovie[];
-}
-
-type TMoviesReducerActions = IReduxGetMoviesAction | IReduxGetMovieAction | IReduxResetMovieAction;
-
-export default function(state: IReduxMoviesState = initialState, action: TMoviesReducerActions) {
-  switch (action.type) {
-    case EReduxActionTypes.GET_MOVIES:
-      return { ...state, movies: action.data, moviesLoaded: true, moviesLoadedAt: Date.now() };
-    case EReduxActionTypes.GET_MOVIE:
-      return { ...state, movie: action.data, movieLoaded: true };
-    case EReduxActionTypes.RESET_MOVIE:
-      return { ...state, movie: undefined, movieLoaded: false };
-    default:
-      return state;
-  }
-} */
-
-
-const activitiesReducer = (activities: any[] = [], action:IReduxBaseAction) => { // need to inialize state(holdings) , holdings is gunna be an array of objects, reducer returns the new state
->>>>>>> 6478484719927c66752cacefacebc2451ea820d5
   switch (action.type) {
     case EReduxActionTypes.FETCH_ACTIVITIES:
       return action.payload;
