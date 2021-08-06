2862c8187f32b6bcc32ba05f66c0663e77325034
Sarah Shaffer <sarahsavell2@gmail.com>
2021-08-06 09:56:07 -0500

    merge

---
+++ Front-end/src/components/Homepage/homepage.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+import React, {useState, useEffect} from 'react';
+import './Homepage.css';
+import NavBar from '../NavBar/navBar';
+import Dashboard from '../Dashboard/Dashboard';
+import Order from '../Order/Order';
+import PastActivities from '../PastActivities/PastActivities';
+import UsersRankings from '../UsersRanking/UsersRanking';
+import News from '../News/News';
+import PacmanLoader from "react-spinners/PacmanLoader";
+
+const Homepage = () => {
+  const [loading, setLoading] = useState(false);
+  const [component, setComponent] = useState('Dashboard');
+  const [title, setTitle] = useState('Practise Investing');
+
+  //Dashboard, Order, Past Acitivities, Ranking
+  const toggleComponent = (componentString) => {
+    setComponent(componentString);
+    setTitle(componentString);
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, [])
+
+  return (
+    <>
+      {
+        loading ? <div className="Homepage"><PacmanLoader color={"blue"} loading={loading} size={45} /></div>
+        :
+          <>
+          <NavBar toggleComponent={toggleComponent} title={title}/>
+          {component==='Dashboard' && <Dashboard />}
+          {component==='Order' && <Order toggleComponent={toggleComponent}/>}
+          {component==='Past Activities' && <PastActivities />}
+          {component==='Ranking' && <UsersRankings />}
+          {component==='News' && <News />}
+          </>
+      }
+    </>
+  );
+};
+
+export default Homepage;
