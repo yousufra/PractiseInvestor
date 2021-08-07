5cfa894cbca93a7f27178e8ec127ce79344e706d
biancahpp <bianca_procopio@hotmail.com>
2021-08-07 11:49:48 -0600

    fix: form verification fix

--- Front-end/src/components/Order/Order.tsx
+++ Front-end/src/components/Order/Order.tsx
@@ -36,9 +36,12 @@ export default function Order({toggleComponent}: Props): ReactElement {
   const [value, setValue] = useState<string>('');
 
   const handleChange = async (company) => {
+    console.log('handle change company', company)
+    // input
     let matches: CompanyStatePropertiesI[] = [];
-    if (company.length > 0) {
+    if (company) {
       matches = (await getMatchingStocks(company)).data;
+      console.log('handle change matches', matches)
     }
     setSuggestions(matches);
   };
@@ -56,7 +59,7 @@ export default function Order({toggleComponent}: Props): ReactElement {
   const handleSubmit = (e: any) => {
     e.preventDefault(); // prevent browser from refreshing , defualt when you submit a form
     // checking if the form is entirely filled
-    if (company && quantity && action) {
+    if (ticker && quantity && action) {
       // checking if the user already owns that holding
       const holding = holdings.find(holding => holding.company === company);
       // checking if the type of action is sell
@@ -77,7 +80,6 @@ export default function Order({toggleComponent}: Props): ReactElement {
         // check cash
         const netAmount: number = Number((price * quantity).toFixed(2));
         if (cash >= netAmount) {
-          console.log('enough funds')
           dispatch(updateHoldings({ date, company, ticker, action, quantity, price, netAmount }));
           setCompany('');
           setTicker('');
@@ -86,7 +88,6 @@ export default function Order({toggleComponent}: Props): ReactElement {
           setPrice(0);
           toggleComponent('Dashboard');
         } else {
-          console.log('not enough funds')
           alert(`Not enough funds`);
         }
       }
