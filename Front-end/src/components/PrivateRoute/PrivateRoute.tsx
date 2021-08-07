9787a20bf8f2563f339370bff2cfed0f89f71ba8
biancahpp <bianca_procopio@hotmail.com>
2021-08-06 19:12:49 -0600

    fix: form validation for order and uuid for mapped elements

--- Front-end/src/components/PrivateRoute/PrivateRoute.tsx
+++ Front-end/src/components/PrivateRoute/PrivateRoute.tsx
@@ -6,19 +6,11 @@ import jwtDecode from 'jwt-decode';
 import { JwtTokenI } from '../../interfaces/JwtToken';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-<<<<<<< HEAD:Front-end/src/components/PrivateRoute/PrivateRoute.js
-  const auth = useSelector((state) => state.authenticate);
-  const [isAuthenticated, setIsAuthenticated] = useState(null);
-  console.log('private route');
-  useEffect(() => {
-    const tokenObject = JSON.parse(localStorage.getItem('home'))
-=======
 
   const auth = useSelector((state: any) => state.authenticate);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
   useEffect(() => {
     const tokenObject: any = localStorage.getItem('home');
->>>>>>> f820d47fc2e2e5a93f2fcf2a46762643f3fac17e:Front-end/src/components/PrivateRoute/PrivateRoute.tsx
     if (tokenObject) {
       const token = JSON.parse(tokenObject).token;
       const tokenExpiration = jwtDecode<JwtTokenI>(token).exp;
