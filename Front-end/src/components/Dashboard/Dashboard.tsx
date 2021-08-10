1ac0b2fc181f712d436c11e56c5483a7031fcdf0
Joseph Kranak <jakranak@gmail.com>
2021-08-10 15:27:44 -0500

    fix: Dashboard capitalization

---
+++ Front-end/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Holdings } from './Holdings/Holdings';
+import { getAllHoldings } from '../../actions/holdings';
+import { PacmanLoader } from 'react-spinners';
+import './Dashboard.css';
+
+interface Props {
+  toggleComponent: (str: string) => void; 
+}
+
+export const Dashboard = ({toggleComponent}: Props) => {
+  
+  const dispatch = useDispatch(); // allows us to dispatch an action
+  const { holdings, cash } = useSelector((state: any) => state.holdings);
+
+  useEffect(() => {  
+    dispatch(getAllHoldings());
+  }, [dispatch]);
+
+  return (
+    (holdings || cash) ?
+    <div>
+      <Holdings toggleComponent={toggleComponent}/>
+    </div>
+    :
+    // if api call takes a little while to load the info, pacman loader will pop up until the api call is done
+    <div className="loader-box">
+      <PacmanLoader color={'blue'} size={45} />
+    </div>
+  )
+}
